158cb75fd9320281438d11e92fcf89290be81c22
yssasaurus <168917783+madicakkes@users.noreply.github.com>
2025-02-28 20:16:08 +0800

    Fixed for docker

--- ISCS3023/WebApp/MSYS22FP/settings.py
+++ ISCS3023/WebApp/MSYS22FP/settings.py
@@ -19,14 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(zl#so&lyit24o2ec#qs2bu83orpx=ukx-g7nk#gojvp)3hw74'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['127.0.0.1:8080', 'localhost']
-
+# The secret key
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ 
+DEBUG = bool(os.environ.get("DEBUG", default=0))
+ 
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 
 # Application definition
 
